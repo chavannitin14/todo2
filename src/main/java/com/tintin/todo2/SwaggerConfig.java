4ad972f202de52014b8b6b453407e264d0ef91c4
Nitin Chavan <83631298+chavannitin14@users.noreply.github.com>
2023-07-21 13:23:19 +0530

    Updated the Swagger config

--- src/main/java/com/tintin/todo2/SwaggerConfig.java
+++ src/main/java/com/tintin/todo2/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.tintin.todo2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,21 +14,20 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.basePackage("com.tintin.todo2"))
+            .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
             .build().apiInfo(apiEndPointInfo());
     }
 
     private ApiInfo apiEndPointInfo() {
         return new ApiInfoBuilder()
-        .title("Todomo Rest Api")
+        .title("Todo2 Rest Api")
         .description("Backend API to support ToDo app")
         .contact(new Contact("Nitin Chavan", "nitinchavan.com/api", "pro.nitin.chavan@hotmail.com"))
         .version("0.0.1")
